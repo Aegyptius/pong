3e9b4b04c3bbf3e5a6755d39a7de7f26c0c16153
Matthew <mwieser@uncc.edu>
2022-08-09 09:59:18 -0400

    Updates to main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from turtle import Turtle, Screen
 
+SCREENWIDTH = 800
+SCREENHEIGHT = 600
+
 screen = Screen()
+screen.screensize(canvwidth=SCREENWIDTH, canvheight=SCREENHEIGHT)
+
+screen.exitonclick()
