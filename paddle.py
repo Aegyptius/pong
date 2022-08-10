5250d23d96c852841debddd4d0a94b7d93675cfb
Matthew <mwieser@uncc.edu>
2022-08-10 13:17:03 -0400

    Updates to main.py and modified paddle class

--- paddle.py
+++ paddle.py
@@ -2,18 +2,17 @@ from turtle import Turtle
 
 STRETCH_WIDTH = 5
 STRETCH_LENGTH = 1
-Y_POSITION = 0
 
 class Paddle:
     #NOTE: To give a class parameter a default value, assign it in the init method parameters
-    def __init__(self, starting_x=350):
+    def __init__(self, starting_x=350, starting_y=0):
         self.paddle = Turtle()
         self.paddle.color("white")
         self.paddle.shape("square")
         self.paddle.speed("fastest")
         self.paddle.shapesize(stretch_wid=STRETCH_WIDTH, stretch_len=STRETCH_LENGTH)
         self.paddle.penup()
-        self.paddle.goto(starting_x, Y_POSITION)
+        self.paddle.goto(starting_x, starting_y)
 
 
     def paddle_up(self):
