51fc43caa6b93a71e7cac17f22bcd39c02fec72f
Matthew <mwieser@uncc.edu>
2022-08-10 12:45:13 -0400

    Updates to main.py and modified paddle class

--- main.py
+++ main.py
@@ -8,11 +8,29 @@ screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=SCREENWIDTH, height=SCREENHEIGHT)
 screen.title("Pong")
+# Tracer method controls the animation. 0 turns off the animation completely
+# Must manually update in order to see object
+screen.tracer(0)
+
+
 
 paddle_right = Paddle()
+paddle_left = Paddle(-350)
+
 
 screen.listen()
-screen.onkey(fun=paddle_right.paddle_up(), key="Up")
+#NOTE: Do not use parentheses when passing function as a parameter
+screen.onkey(fun=paddle_right.paddle_up, key="Up")
+screen.onkey(fun=paddle_right.paddle_down, key="Down")
+screen.onkey(fun=paddle_left.paddle_up, key="w")
+screen.onkey(fun=paddle_left.paddle_down, key="s")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
+
+
 
 
 
--- paddle.py
+++ paddle.py
@@ -2,18 +2,29 @@ from turtle import Turtle
 
 STRETCH_WIDTH = 5
 STRETCH_LENGTH = 1
-X_POSITION = 350
 Y_POSITION = 0
 
 class Paddle:
-    def __init__(self):
+    #NOTE: To give a class parameter a default value, assign it in the init method parameters
+    def __init__(self, starting_x=350):
         self.paddle = Turtle()
         self.paddle.color("white")
         self.paddle.shape("square")
         self.paddle.speed("fastest")
         self.paddle.shapesize(stretch_wid=STRETCH_WIDTH, stretch_len=STRETCH_LENGTH)
         self.paddle.penup()
-        self.paddle.goto(X_POSITION, Y_POSITION)
+        self.paddle.goto(starting_x, Y_POSITION)
+
 
     def paddle_up(self):
-        self.paddle.forward(20)
+        if self.paddle.ycor() >= 240:
+            pass
+        else:
+            self.paddle.goto(self.paddle.xcor(), self.paddle.ycor() + 20)
+
+
+    def paddle_down(self):
+        if self.paddle.ycor() <= -240:
+            pass
+        else:
+            self.paddle.goto(self.paddle.xcor(), self.paddle.ycor() - 20)
